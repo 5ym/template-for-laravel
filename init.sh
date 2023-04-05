b263afd42dedcf47fd5875ebe6f494a41ed27fce
Ruk <info@doany.io>
2023-04-05 07:53:56 +0000

    fix

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/5ym/docker-laravel-jit.git
 cd docker-laravel-jit
 cp docker-compose.sample.yml docker-compose.yml
-docker run --rm --network host -v ./html:/app composer create-project laravel/laravel .
+docker run --rm --network host -v $(pwd)/html:/app composer create-project laravel/laravel .
 docker compose run --rm -u root app sh -c "chown -R www-data:www-data . && chmod -R 777 . && sed -i -e 's/.env$//g' .gitignore"
 sed -i -e 's/\/html//g' .gitignore
 docker compose up
